95f1e468c008a55ea10d45101764a855960bebad
RC_Student_lab <ndoth52@gmail.com>
2024-09-28 22:53:24 +0000

    Message

--- app/src/main/java/com/example/tamagotchi20/Sec_Activity.kt
+++ app/src/main/java/com/example/tamagotchi20/Sec_Activity.kt
@@ -23,41 +23,41 @@ class Sec_Activity : AppCompatActivity() {
         buttonP= findViewById(R.id.button_p)
         image= findViewById(R.id.imageView)
 
-            //Upon clicking the Feed button
-            buttonF.setOnClickListener {
+        //Upon clicking the Feed button
+        buttonF.setOnClickListener {
 
-                if (image.tag == "welcome" || image.tag == "playing" || image.tag == "bath")
-                {
-                    image.setImageResource(R.drawable.feeding)
-                    image.tag = "feeding"
-                } else {
-                    image.setImageResource(R.drawable.welcome)
-                    image.tag = "welcome"
-                }
+            if (image.tag == "welcome" || image.tag == "playing" || image.tag == "bath")
+            {
+                image.setImageResource(R.drawable.feeding)
+                image.tag = "feeding"
+            } else {
+                image.setImageResource(R.drawable.welcome)
+                image.tag = "welcome"
             }
-            //Upon clicking the clean button
-            buttonC.setOnClickListener {
+        }
+        //Upon clicking the clean button
+        buttonC.setOnClickListener {
 
-                if (image.tag == "welcome" || image.tag == "feeding" || image.tag == "playing")
-                {
-                    image.setImageResource(R.drawable.bath)
-                    image.tag = "bath"
-                } else {
-                    image.setImageResource(R.drawable.welcome)
-                    image.tag = "welcome"
-                }
+            if (image.tag == "welcome" || image.tag == "feeding" || image.tag == "playing")
+            {
+                image.setImageResource(R.drawable.bath)
+                image.tag = "bath"
+            } else {
+                image.setImageResource(R.drawable.welcome)
+                image.tag = "welcome"
             }
-            //Upon clicking the play button
-            buttonP.setOnClickListener {
+        }
+        //Upon clicking the play button
+        buttonP.setOnClickListener {
 
-                        if (image.tag == "welcome" || image.tag == "bath" || image.tag == "feeding")
-                        {
-                            image.setImageResource(R.drawable.playing)
-                            image.tag = "playing"
-                        } else {
-                            image.setImageResource(R.drawable.welcome)
-                            image.tag = "welcome"
-                        }
+                    if (image.tag == "welcome" || image.tag == "bath" || image.tag == "feeding")
+                    {
+                        image.setImageResource(R.drawable.playing)
+                        image.tag = "playing"
+                    } else {
+                        image.setImageResource(R.drawable.welcome)
+                        image.tag = "welcome"
+                    }
             }
 
 
